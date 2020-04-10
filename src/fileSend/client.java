89eb8b6e91a4f2e52efe69f7e9f2df9ca92dfae2
admin <2608656305@qq.com>
2020-04-10 15:42:12 +0800

    初步完成，待解决：1、双向                  2、不用每一个文件一次链接

--- src/fileSend/client.java
+++ src/fileSend/client.java
@@ -1,19 +1,27 @@
 package fileSend;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public  class client{
     public static void main(String []args){
-        new MyFrame("文件传输server");
+        new MyFrame("文件传输client");
     }
 
 }
 
-class MyFrame extends JFrame implements ActionListener{
+class MyFrame extends JFrame {
+    File sendFile = null;
+    File saveFile = null;
+    Socket client=null;
+    Thread ClientThread=null;
     TextField SendPath = new  TextField(15);
     TextField SavePath = new  TextField("C:\\Users",15);
     JButton Send = new JButton("Send");
@@ -23,6 +31,7 @@ class MyFrame extends JFrame implements ActionListener{
     JButton chooseSend = new JButton("选择");
     JButton chooseSave = new JButton("选择");
     JButton connect = new JButton("连接服务");
+    Listener listener=new Listener();
     public MyFrame(String title){
         super(title);
         setLocation(10,10);		//窗体位置
@@ -37,6 +46,7 @@ class MyFrame extends JFrame implements ActionListener{
         North.add(port);
         North.add(connect);
         this.add(North,BorderLayout.NORTH);
+        connect.addActionListener(this.listener);
         //CENTER
         Panel Center = new Panel();
         Center.add(sfTA);
@@ -46,8 +56,8 @@ class MyFrame extends JFrame implements ActionListener{
         Center.add(new JLabel("保存文件路径："));
         Center.add(SavePath);
         Center.add(chooseSave);
-        chooseSave.addActionListener(this);
-        chooseSend.addActionListener(this);
+        chooseSave.addActionListener(this.listener);
+        chooseSend.addActionListener(this.listener);
 
         this.add(Center,BorderLayout.CENTER);
         //SOUTH
@@ -56,33 +66,172 @@ class MyFrame extends JFrame implements ActionListener{
         this.add(South,BorderLayout.SOUTH);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Send.addActionListener(this.listener );
+    }
+    class Listener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if(actionEvent.getSource()==chooseSend){
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                chooser.showDialog(new JLabel(), "选择");
+                sendFile = chooser.getSelectedFile();
+                SendPath.setText(sendFile.getAbsoluteFile().toString());
+
+            }else  if(actionEvent.getSource()==chooseSave){
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                chooser.showDialog(new JLabel(), "选择");
+                saveFile = chooser.getSelectedFile();
+                SavePath.setText(saveFile.getAbsoluteFile().toString());
+            }else if (actionEvent.getSource()==connect){
+                System.out.println("进入connect");
+                sfTA.append("Socket Connect IP :"+adress.getText()+"\nConnect Port :"+port.getText()+"\n");
+                try {
+//                client =new Socket(adress.getText(),Integer.parseInt(port.getText()));
+                    client =new Socket(InetAddress.getLocalHost(),8000);
+                    sfTA.append("connect ...\n");
+                    ClientThread=new Thread(new clientThread(client));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (actionEvent.getSource()==Send){
+                System.out.println((ClientThread==null));
+                System.out.println("进入send");
+
+                ClientThread.start();
+
+            }
+        }
+    }
+    class clientThread implements Runnable{
+        private Socket client=null;
+
+        public clientThread(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            if(sendFile==null){
+                JOptionPane.showMessageDialog(null, "请选择发送文件", "警告", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            try {
+                System.out.println("进入send1");
+
+                OutputStream clientOut = client.getOutputStream();
+                InputStream clientIn = client.getInputStream();
+                System.out.println("进入send2");
+
+                byte[] filename;
+                filename = sendFile.getName().getBytes();
+                System.out.println("进入send3");
+
+                clientOut.write(filename);
+                clientOut.flush();
+                System.out.println("进入send4");
+                byte[] nameIn=new byte[1024];
+                int len =clientIn.read(nameIn);
+                System.out.println("进入send5");
+
+                if(!"accept".equals(new String(nameIn,0,len))){
+                    JOptionPane.showMessageDialog(null, "服务器拒绝接收文件", "失败", JOptionPane.ERROR_MESSAGE);
+                    clientIn.close();
+                    clientOut.close();
+                    return;
+                }
+                System.out.println("进入send6");
+
+                InputStream in = new FileInputStream(sendFile);
+                byte[] data = new byte[1024];
+                int data_len=0;
+                while ((data_len=in.read(data))!=-1){
+                    clientOut.write(data,0,data_len);
+                }
+                sfTA.append("文件发送完毕\n");
+
+
+                in.close();
+                clientIn.close();
+                clientOut.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        if(actionEvent.getSource()==chooseSend){
-            JFileChooser chooser = new JFileChooser();
-            chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            chooser.showDialog(new JLabel(), "选择");
-            File file = chooser.getSelectedFile();
-            SendPath.setText(file.getAbsoluteFile().toString());
-            if(file.isDirectory())
-                SendPath.setText("选择有效文件！");
-            else if (file.isFile())
-                SendPath.setText(file.getParentFile().toString());
+    }
 
-        }else  if(actionEvent.getSource()==chooseSave){
-            JFileChooser chooser = new JFileChooser();
-            chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            chooser.showDialog(new JLabel(), "选择");
-            File file = chooser.getSelectedFile();
-            if(file.isDirectory())
-                SavePath.setText(file.getAbsoluteFile().toString());
-            else if (file.isFile())
-                SavePath.setText(file.getParentFile().toString());
+class FileUtil1{
+    private File srcFile;
+    private File desFile;
+    public FileUtil1(String srcFile,String desFile){
+        this.desFile=new File(desFile);
+        this.srcFile=new File(srcFile);
+    }
+    public boolean copy(){
+        if(!this.srcFile.exists()) {
+            System.out.println("源文件不存在");
+            return false;
+        }
 
 
+        return copyFileImpl(this.srcFile,this.desFile);
+
+    }
+    public boolean copyFileImpl(File srcFile,File desFile){
+        if(!this.desFile.getParentFile().exists())
+            this.desFile.getParentFile().mkdirs();
+        byte[] data =new byte[1024];
+        InputStream input=null;
+        OutputStream output =null;
+        try {
+            input=new FileInputStream(srcFile);
+            output=new FileOutputStream(desFile);
+            int len=0;
+            while ((len=input.read(data))!=-1){
+                output.write(data,0,len);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(input!=null)
+                    input.close();
+                if (output!=null)
+                    output.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return true;
+    }
+    public void copyImpl(File file){
+        if(file.isDirectory()){
+            File[] result = file.listFiles();
+            if(result!=null){
+                for(int n=0;n<result.length;n++)
+                {
+                    copyImpl(result[n]);
+                }
+            }
+        }else {
+            String newfilePath = file.getPath().replace(this.srcFile.getPath()+File.separator,"");
+            File newFile = new File(this.desFile,newfilePath);
+
+            copyFileImpl(file,newFile);
         }
 
     }
+    public  boolean copyDir(){
+        this.copyImpl(this.srcFile);
+        return true;
+    }
+
 }
--- src/fileSend/server.java
+++ src/fileSend/server.java
@@ -3,32 +3,34 @@ package fileSend;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 public  class server{
-    public static void main(String []args){
+    public static void main(String []args) throws IOException {
         new FileChooser("文件传输server");
+
     }
 
 }
- class FileChooser extends JFrame implements ActionListener{
+ class FileChooser extends JFrame {
+     File saveFile = null;
+     File sendFile = null;
     ServerSocket server=null;
-    Socket client =null;
      Thread ServerThread=null;
-     TextField path = new  TextField(20);
-    JButton send = new JButton("Send");
-    TextArea sfTA = new TextArea(14,  35);
-    TextField port = new  TextField("8000");
-     JButton choose = new JButton("选择");
+
+     TextField SendPath = new  TextField(15);
+     TextField SavePath = new  TextField("C:\\Users\\admin\\Desktop\\新建文件夹",15);
+     TextField port = new  TextField("8000");
+     TextArea sfTA = new TextArea(14,  35);
+     JButton send = new JButton("Send");
+     JButton chooseSend = new JButton("选择");
+     JButton chooseSave = new JButton("选择");
      JButton connect = new JButton("运行服务");
+     CFListener listener = new CFListener();
     public FileChooser(String title){
         super(title);
         setLocation(10,10);		//窗体位置
@@ -41,13 +43,18 @@ public  class server{
         North.add(port);
         North.add(connect);
         this.add(North,BorderLayout.NORTH);
+        connect.addActionListener(this.listener);
         //CENTER
         Panel Center = new Panel();
         Center.add(sfTA);
-        Center.add(new JLabel("文件路径："));
-        Center.add(path);
-        choose.addActionListener(this);
-        Center.add(choose);
+        Center.add(new JLabel("发送文件路径："));
+        Center.add(SendPath);
+        Center.add(chooseSend);
+        Center.add(new JLabel("保存文件路径："));
+        Center.add(SavePath);
+        Center.add(chooseSave);
+        chooseSend.addActionListener(this.listener);
+        chooseSave.addActionListener(this.listener);
         this.add(Center,BorderLayout.CENTER);
         //SOUTH
         Panel South = new Panel();
@@ -57,38 +64,97 @@ public  class server{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-     @Override
-     public void actionPerformed(ActionEvent actionEvent) {
 
-         if(actionEvent.getSource()==choose){
-             JFileChooser chooser = new JFileChooser();
-             chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-             chooser.showDialog(new JLabel(), "选择");
-             File file = chooser.getSelectedFile();
-             path.setText(file.getAbsoluteFile().toString());
-         }else if(actionEvent.getSource()==connect){
-             try {
-                 connect.setEnabled(false);
-                 int i = Integer.parseInt(port.getText());
-                 sfTA.append("Server Port :"+port.getText()+"\n");
-                 server = new ServerSocket(i);
-                 client = server.accept();
-                 sfTA.append("a client connect ...\n");
-                 this.ServerThread  = new Thread(new SeverProcessThread());  //
-                 ServerThread.start();
-             } catch (IOException e) {
-                 e.printStackTrace();
+     class CFListener implements ActionListener {
+
+         @Override
+         public void actionPerformed(ActionEvent actionEvent) {
+             if(actionEvent.getSource()==chooseSend){
+                 JFileChooser chooser = new JFileChooser();
+                 chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                 chooser.showDialog(new JLabel(), "选择");
+                 sendFile = chooser.getSelectedFile();
+                 SendPath.setText(sendFile.getAbsoluteFile().toString());
+             }else  if(actionEvent.getSource()==chooseSave){
+                 JFileChooser chooser = new JFileChooser();
+                 chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                 chooser.showDialog(new JLabel(), "选择");
+                 saveFile = chooser.getSelectedFile();
+                 SavePath.setText(saveFile.getAbsoluteFile().toString());
+             }else if(actionEvent.getSource()==connect){
+                 try {
+                      connect.setEnabled(false);
+                     server = new ServerSocket(8000);
+                     sfTA.append("Server Port :"+port.getText()+"\n");
+                     int i = Integer.parseInt(port.getText());
+                     sfTA.append("Server Port :"+port.getText()+"\n");
+                     ServerThread  = new Thread(new SeverProcessThread(server));  //
+                     ServerThread.start();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+
              }
+         }
+     }
+     class SeverProcessThread implements Runnable{
 
-         }else if(actionEvent.getSource()==send){
+        ServerSocket server=null;
+
+         public SeverProcessThread( ServerSocket server) {
+
+             this.server = server;
 
          }
 
+         @Override
+         public void run() {
+             while (true) {
+                 try {
+                     Socket client= server.accept();
+                     sfTA.append("a client connect ...\n");
+                    InputStream serverIn = client.getInputStream();
+                     OutputStream serverOut =client.getOutputStream();
+                     byte[] fname = new byte[1024];
+                     System.out.println("进入accpet");
+
+                     int len = serverIn.read(fname);
+                     System.out.println("进入1");
+
+                     String fileName = new String(fname, 0, len);
+                     int isAccept = JOptionPane.showConfirmDialog(null, "是否接受发来文件：" + fileName, "新文件", JOptionPane.YES_NO_OPTION);
+                     System.out.println("进入2");
+
+                     if (isAccept == JOptionPane.NO_OPTION) {
+                         serverOut.write("noaccept".getBytes());
+
+                         serverIn.close();
+                         serverOut.close();
+                     }
+                     System.out.println("进入3");
+                     serverOut.write("accept".getBytes());
+
+                     saveFile = new File(saveFile, fileName);
+                   OutputStream out = new FileOutputStream(saveFile);
+                     byte[] data = new byte[1024 * 1024];
+                     int datalen = 0;
+                     while ((datalen = serverIn.read(data)) != -1) {
+                         System.out.println("进入4");
+                         out.write(data, 0, datalen);
+
+                     }
+                     sfTA.append("文件传输完毕\n");
+                     saveFile=saveFile.getParentFile();
+
+
+                     serverIn.close();
+                     serverOut.close();
+                     out.close();
+                     client.close();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             }
+         }
      }
  }
-class SeverProcessThread implements Runnable{
-    @Override
-    public void run() {
-
-    }
-}
